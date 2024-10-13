0787c7b673366091d9f1b09a974af6936bc87e4e
Stephen Kitomary <kitomarystephen@gmail.com>
2024-10-13 13:59:31 -0400

    updates

--- VM Translator/VMtranslatorV1.py
+++ VM Translator/VMtranslatorV1.py
@@ -1,39 +1,69 @@
 import os
-def main():
-    # we are going to take a case sensitive file name from the user, add .vm extension to it and then look it up in the directory
-    input_file = input("Enter the finename that you want to translate (Case Sensitive): ")
-    vm_file = input_file + ".vm"
-    asm_file = input_file + ".asm"
-    #lets first check if the type file name really exists
-    if not os.path.isfile(vm_file):
-        raise FileNotFoundError("The file you specified does not exist, please make sure it exists in the same directory as this code")
-    
-    #lets open the input file as read, and create an output file with write priviledges
-    with open(vm_file, 'r') as file, open(asm_file,'w') as output:
-        for line in file:
-            stripped_line = line.strip()
-            if stripped_line.startswith("//") or stripped_line == "":
-                continue
-            command_type, command_elements = parser(stripped_line)
-            code_writer(output, command_type, command_elements, stripped_line)
 
-    print("Translation has been completed")
- #Lets create a parser function that takes a single line as command and splits it into its elements
+def main():
+    # we are going to take a case-sensitive filename or directory from the user
+    input_path = input("Enter the filename or directory you want to translate (Case Sensitive): ")
+
+    # Check if the input is a file or directory
+    if os.path.isfile(input_path):
+        vm_files = [input_path]  # Single file
+    elif os.path.isdir(input_path):
+        # If it's a directory, gather all .vm files in the directory
+        vm_files = [os.path.join(input_path, f) for f in os.listdir(input_path) if f.endswith('.vm')]
+        if not vm_files:
+            raise FileNotFoundError("No .vm files found in the specified directory.")
+    else:
+        raise FileNotFoundError("The specified file or directory does not exist.")
+
+    # Determine the output .asm file name
+    if os.path.isfile(input_path):
+        # If it's a file, name the output file based on the input file name
+        asm_file = os.path.splitext(input_path)[0] + ".asm"
+    else:
+        # If it's a directory, name the output file after the directory
+        asm_file = os.path.join(input_path, os.path.basename(input_path) + ".asm")
+
+    # Open the output file
+    with open(asm_file, 'w') as output:
+        #Write the bootsrap code
+        output.write("// Bootstrap Code\n")
+        output.write(boostrap_code())
+        call_index = 0
+        # Process each .vm file
+        for vm_file in vm_files:
+            print(f"Processing {vm_file}...")
+            with open(vm_file, 'r') as file:
+                for line in file:
+                    stripped_line = line.strip()
+                    if stripped_line.startswith("//") or stripped_line == "":
+                        continue
+                    # Parse the command and write the assembly code
+                    command_type, command_elements = parser(stripped_line)
+                    code_writer(output, command_type, command_elements, stripped_line, call_index)
+                    if command_type == "function" and command_elements[0] == "call":
+                        call_index += 1
+
+    print("Translation has been completed. Output written to:", asm_file)
  
 def parser(command):
     elements = command.split()
-    # lets determine if the command is either a memory segmentation command or an Arithmentic command or an invalid command for now,
-    # later should modify this to take in Branching commands 
     if elements[0] in ["push","pop"]:
         command_type =  "memory"
         return command_type, elements
     elif elements[0] in ["add","sub","neg","eg","gt","lt","and","or","not"]:
         command_type = "arithmetic"
         return command_type, elements
+    elif elements[0] in ["label", "goto", "if-goto"]:
+        command_type = "branching"
+        return command_type, elements
+    elif elements[0] in ["function", "call", "return"]:
+        command_type = "function"
+        return command_type, elements
     else:
-        raise ValueError("Unknown command found")
+        command_type = "invalid"
+        return command_type, elements
 
-def code_writer(output, command_type, elements, original_command):
+def code_writer(output, command_type, elements, original_command,call_index):
     #lets first write a comment that is the VM command on top before the ASM code, will be helpful in troubleshooting
     output.write(f"//{original_command}\n")
 
@@ -46,39 +76,87 @@ def code_writer(output, command_type, elements, original_command):
     
     elif command_type == "arithmetic":
         output.write(arithmetic_command(elements[0]))
-#here goes our push command operations, universal for all segment types except constant
+    elif command_type == "branching":
+        operation = elements[0]
+        label = elements[1]
+        if operation == "label":
+            output.write(f"({label})\n")
+        elif operation == "goto":
+            output.write(goto_command(label))
+        elif operation == "if-goto":
+            output.write(if_goto_command(label))
+    elif command_type == "function":
+        operation = elements[0]
+        if operation == "function":
+            function_name = elements[1]
+            n_vars = elements[2]
+            output.write(function_function(function_name, n_vars))
+        elif operation == "call":
+            function_name = elements[1]
+            n_args = elements[2]
+            output.write(call_function(function_name, n_args, call_index))
+        elif operation == "return":
+            output.write(return_command())
+
+    
 def push_command(segment, index):
     if segment == "constant":
         return f"@{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    else:
-        segment_map = {
-        "local": "LCL",
-        "argument": "ARG",
-        "this": "THIS",
-        "that": "THAT"
-    }
-        asm_segment = segment_map.get(segment)
-        return f"@{index}\nD=A\n@{asm_segment}\nA=M+D\nD=M\n@{asm_segment}\nA=M\nM=D\n@SP\nM=M+1\n"
-
     
+    elif segment in ["local", "argument", "this", "that"]:
+        segment_map = {
+            "local": "LCL",
+            "argument": "ARG",
+            "this": "THIS",
+            "that": "THAT"
+        }
+        asm_segment = segment_map[segment]
+        return (f"@{index}\nD=A\n@{asm_segment}\nA=D+M\nD=M\n"
+                "@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+
+    elif segment == "pointer":
+        if index == "0":
+            return "@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+        elif index == "1":
+            return "@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    
+    elif segment == "temp":
+        temp_address = 5 + int(index)
+        return f"@{temp_address}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+
     return ""
 
-#here goes our pop command operations
-def pop_command(segment, index):
-    segment_map = {
-        "local": "LCL",
-        "argument": "ARG",
-        "this": "THIS",
-        "that": "THAT"
-    }
-    asm_segment = segment_map.get(segment)
 
-    return f"@{index}\nD=A\n@{asm_segment}\nD=D+M\n@SP\nA=M\nM=D\n@SP\nA=M-1\nD=M\n@SP\nA=M\nA=M\nM=D\n@SP\nM=M-1\n"  
-#here goes our arithmetic operations translations
+def pop_command(segment, index):
+    if segment in ["local", "argument", "this", "that"]:
+        segment_map = {
+            "local": "LCL",
+            "argument": "ARG",
+            "this": "THIS",
+            "that": "THAT"
+        }
+        asm_segment = segment_map[segment]
+        return (
+            f"@{index}\nD=A\n@{asm_segment}\nD=M+D\n@R13\nM=D\n"  
+            "@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"                  
+        )
+
+    elif segment == "pointer":
+        if index == "0":
+            return "@SP\nAM=M-1\nD=M\n@THIS\nM=D\n"
+        elif index == "1":
+            return "@SP\nAM=M-1\nD=M\n@THAT\nM=D\n"
+
+    elif segment == "temp":
+        temp_address = 5 + int(index)
+        return f"@SP\nAM=M-1\nD=M\n@{temp_address}\nM=D\n"
+
+    return ""
+
 def arithmetic_command(operation):
     
     if operation == "add":
-        return "@SP\nAM=M-1\nD=M\nA=A-1\nM=M+D\n"
+        return "@SP\nAM=M-1\nD=M\nA=A-1\nM=D+M\n"
     elif operation == "sub":
         return "@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D\n"
     elif operation == "neg":
@@ -99,43 +177,75 @@ def arithmetic_command(operation):
     return ""
 
 # Just for layout, writting all the big/long functions here as separate functions that will get called inside the arithmetic_command function
+comparison_count = 0  # Add this as a class-level variable or within your function's scope
+
 def eq_command():
-    return ("@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n"
-            "@ISTRUE\nD;JEQ\nD=0\n@ISFALSE\n0;JMP\n"
-            "(ISTRUE)\nD=-1\n(ISFALSE)\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+    global comparison_count
+    comp_label = comparison_count
+    comparison_count += 1
+    return (f"@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n"
+            f"@TRUE{comp_label}\nD;JEQ\nD=0\n@FALSE{comp_label}\n0;JMP\n"
+            f"(TRUE{comp_label})\nD=-1\n(FALSE{comp_label})\n"
+            "@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+
 def gt_command():
-    return ("@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n"
-            "@ISTRUE\nD;JGT\nD=0\n@ISFALSE\n0;JMP\n"
-            "(ISTRUE)\nD=-1\n(ISFALSE)\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+    global comparison_count
+    comp_label = comparison_count
+    comparison_count += 1
+    return (f"@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n"
+            f"@TRUE{comp_label}\nD;JGT\nD=0\n@FALSE{comp_label}\n0;JMP\n"
+            f"(TRUE{comp_label})\nD=-1\n(FALSE{comp_label})\n"
+            "@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+
 def lt_command():
-    return ("@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n"
-            "@ISTRUE\nD;JLT\nD=0\n@ISFALSE\n0;JMP\n"
-            "(ISTRUE)\nD=-1\n(ISFALSE)\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+    global comparison_count
+    comp_label = comparison_count
+    comparison_count += 1
+    return (f"@SP\nAM=M-1\nD=M\n@SP\nAM=M-1\nD=M-D\n"
+            f"@TRUE{comp_label}\nD;JLT\nD=0\n@FALSE{comp_label}\n0;JMP\n"
+            f"(TRUE{comp_label})\nD=-1\n(FALSE{comp_label})\n"
+            "@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+
 
 def return_command():
     return ("@LCL\nD=M\n@R13\nM=D\n@5\nA=D-A\nD=M\n"
-            "@R14\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@ARG\nA=M\nM=D\n"
-            "@ARG\n \n@SP\nM=D+1\n"
-            "@R13\nM=M-1\nA=M\nD=M\n@THAT\nM=D\n"
-            "@R13\nM=M-1\nA=M\nD=M\n@THIS\nM=D\n"
-            "@R13\nM=M-1\nA=M\nD=M\n@ARG\nM=D\n"
-            "@R13\nM=M-1\nA=M\nD=M\n@LCL\nM=D\n"
-            "@R14\nA=M\nO;JMP\n")
-def call_function():# still have to figure out way to get the return adress
-    return (f"@{return_address}\nD=A\n@SP\nM=D\n@SP\nM=M+1\n"
+            "@R14\nM=D\n@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D\n"
+            "@ARG\nD=M+1\n@SP\nM=D\n"
+            "@R13\nAM=M-1\nD=M\n@THAT\nM=D\n"
+            "@R13\nAM=M-1\nD=M\n@THIS\nM=D\n"
+            "@R13\nAM=M-1\nD=M\n@ARG\nM=D\n"
+            "@R13\nAM=M-1\nD=M\n@LCL\nM=D\n"
+            "@R14\nA=M\n0;JMP\n")
+def call_function(function_name, n_args, call_index):
+    return_address = f"{function_name}$ret.{call_index}"
+    return (f"@{return_address}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
             "@LCL\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
             "@ARG\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
             "@THIS\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
             "@THAT\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-            "@SP\nD=M\n@LCL\nM=D"
-            f"@5\nD=D-A\n@{n_args}\nD=D-A\n@ARD\nM=D\n"
-            f"@{function_name}\n0;JMP\n({return_address})")
-def function_function():
+            "@SP\nD=M\n@LCL\nM=D\n"
+            f"@5\nD=D-A\n@{n_args}\nD=D-A\n@ARG\nM=D\n"
+            f"@{function_name}\n0;JMP\n({return_address})\n")
+def function_function(function_name,n_vars):
+    assembly_code = f"({function_name})\n"
     for i in range(int(n_vars)):
-            ["@SP", "A=M", "M=0", "@SP", "M=M+1"]
-    return (f"({function_name})")
+        assembly_code += "@SP\nA=M\nM=0\n@SP\nM=M+1\n" 
+    return assembly_code
+def if_goto_command(label):
+    return (f"@SP\nM=M-1\nA=M\n@{label}\nD;JNE\n")
 
+def goto_command(label):
+    return (f"@{label}\n0;JMP\n")
 
+def boostrap_code():
+ 
+    return (
+        "@256\n"        
+        "D=A\n"
+        "@SP\n"
+        "M=D\n"         
+        f"{call_function('Sys.init', 0, 0)}"  
+    )
 #main process
 if __name__ == "__main__":
     main()
